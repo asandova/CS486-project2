4039c150f69a493731629c1e8e057ceea5779714
asandova <asandova@nmsu.edu>
2018-04-04 00:07:52 -0600

    added implementation of create_deBrujn_graph_by_hashing Needs to be tested

--- CS486-project2/deBruijnByHash.cpp
+++ CS486-project2/deBruijnByHash.cpp
@@ -6,6 +6,14 @@
 //  Copyright © 2015 Joe Song. All rights reserved.
 //
 //  Updated 3/19/2018
+/*
+*	Editied by: August B. Sandoval, 
+*	Class: CS486
+*	Assignment: Project 2
+*
+*/
+
+
 
 #include "k-assembler.hpp"
 
@@ -94,38 +102,69 @@ CSeqHash create_hash_table(const vector<string> & kmers)
 void create_deBruijn_graph_by_hashing(const vector<string> & kmers, DiGraph & g)
 // create a deBruijn graph by inserting all k-mers into the graph by hashing
 {
-    // TO DO:
-    
-    // BEGIN your code below:
-    
-    // create one hash table for both the k-1 prefix and suffix of
-    //   each k-mer
-    
-    // initialize an empty node vector for graph g
-    
-    
-    // for each k-mer
-    
-        // find the prefix node id from_id from the hash table
-        
+	// TO DO:
 
-        // update node from_id's label to prefix if necessary
+	// BEGIN your code below:
 
-    
-        // find the suffix node id to_id from the hash table
-        
-    
-        // update node to_id's label to suffix if necessary
-    
-    
-        // create a new edge (from_id, to_id) by inserting node
-        //   to_id into the adjaceny list of node from_id
+	// create one hash table for both the k-1 prefix and suffix of
+	//   each k-mer
+	CSeqHash NTable = create_hash_table(kmers);
+	// initialize an empty node vector for graph g
+	vector<Node> KNodes = vector<Node>();
+	// for each k-mer
+	for (vector<string>::const_iterator itr = kmers.begin(); itr != kmers.end(); ++itr) {
+		string preKey = itr->substr(0, itr->size() - 1);
+		string sufKey = itr->substr(1, itr->size() - 1);
+		
+		// find the prefix node id from_id from the hash table
+		size_t pre = NTable.find(preKey)->second;
+		// update node from_id's label to prefix if necessary
+		size_t prePos, sufPos;
+		if (pre == NTable.end) {
+			Node from_id;
+			from_id.m_label == preKey;
+			from_id.m_num_of_incoming = 0;
+			KNodes.push_back(from_id);
+			prePos = KNodes.size()-1;
+		} else{
+			for (prePos = 0; prePos < KNodes.size(); prePos++) {
+				if (KNodes[prePos].m_label == preKey) {
+					break;
+				}
+			}
+		}
+		// find the suffix node id to_id from the hash table
+		size_t suf = NTable.find(sufKey)->second;
+		// update node to_id's label to suffix if necessary
+		if (suf == NTable.end) {
+			Node to_id;
+			to_id.m_label = sufKey;
+			to_id.m_num_of_incoming = 0;
+			KNodes.push_back(to_id);
+			sufPos = KNodes.size() - 1;
+		}else {
+			for (sufPos = 0; sufPos < KNodes.size(); sufPos++) {
+				if (KNodes[sufPos].m_label == sufKey) {
+					break;
+				}
+			}
+		}
+		// create a new edge (from_id, to_id) by inserting node
+		//   to_id into the adjaceny list of node from_id
+		KNodes[prePos].m_outgoing.push_back( sufPos );
+		KNodes[sufPos].m_num_of_incoming++;
 
-    
-        // update the number of incoming edges of node to_id
-    
+		// update the number of incoming edges of node to_id
+	}
     // end for loop
-    
+	g.m_nodes.resize(KNodes.size());
+	//vector<Node>::iterator litr = KNodes.begin();
+	//vector<Node>::iterator virt = g.m_nodes.begin();
+	auto litr = KNodes.begin();
+	auto vitr = g.m_nodes.begin();
+	while (litr != KNodes.end()) {
+		*vitr++ = *litr++;
+	}
     // END your code above
 
 }
